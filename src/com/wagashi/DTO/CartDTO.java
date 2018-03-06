01db8cb89c387afc1b5d3038923a55a6a3d4103b
kj-java <10koji25@gmail.com>
2018-03-06 12:45:48 +0900

    c

---
+++ src/com/wagashi/DTO/CartDTO.java
@@ -0,0 +1,55 @@
+package com.wagashi.DTO;
+
+public class CartDTO {
+
+	private String userId;
+
+	private String tempUserId;
+
+	private int productId;
+
+	private int productCount;
+
+	private int price;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getTempUserId() {
+		return tempUserId;
+	}
+
+	public void setTempUserId(String tempUserId) {
+		this.tempUserId = tempUserId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getProductCount() {
+		return productCount;
+	}
+
+	public void setProductCount(int productCount) {
+		this.productCount = productCount;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
---
+++ src/com/wagashi/DTO/ProductInfoDTO.java
@@ -0,0 +1,85 @@
+package com.wagashi.DTO;
+
+public class ProductInfoDTO {
+
+	private int productId;
+
+	private String productName;
+
+	private String productNameKana;
+
+	private String productDescription;
+
+	private String categoryId;
+
+	private int price;
+
+	private String imageFilePath;
+
+	private String imageFileName;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductNameKana() {
+		return productNameKana;
+	}
+
+	public void setProductNameKana(String productNameKana) {
+		this.productNameKana = productNameKana;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+}
