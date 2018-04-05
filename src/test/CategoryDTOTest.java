3772dfa0b3c539c826a397bc3dfd291c898e5274
User <User@192.168.1.33>
2018-04-05 14:37:52 +0900

    aa

--- src/test/CategoryDTOTest.java
+++ src/test/CategoryDTOTest.java
@@ -4,66 +4,407 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.wagashi.DTO.CategoryDTO;
+
 public class CategoryDTOTest {
 
 	@Test
-	public void testGetId() {
-		fail("まだ実装されていません");
+	public void testGetId1() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected =null;
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetId() {
-		fail("まだ実装されていません");
+	public void testGetId2() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetCategoryId() {
-		fail("まだ実装されていません");
+	public void testGetId3() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected =" ";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetCategoryId() {
-		fail("まだ実装されていません");
+	public void testGetId4() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="　";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetCategoryName() {
-		fail("まだ実装されていません");
+	public void testGetId5() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetCategoryName() {
-		fail("まだ実装されていません");
+	public void testGetId6() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="あいう１２３";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetCategoryDescription() {
-		fail("まだ実装されていません");
+	public void testGetId7() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetCategoryDescription() {
-		fail("まだ実装されていません");
+	public void testGetId8() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setId(expected);
+		String actual=dto.getId();
+
+		assertEquals(expected,actual);
+
+	}
+
+
+	@Test
+	public void testGetCategoryId1() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="null";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId2() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId3() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected =" ";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId4() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="　";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId5() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId6() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="あいう１２３";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId7() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testGetCategoryId8() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setCategoryId(expected);
+		String actual = dto.getCategoryId();
+
+		assertEquals(expected,actual);
+	}
+
+
+	@Test
+	public void testGetCategoryName1() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "null";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName2() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName3() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = " ";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName4() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "　";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName5() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "abc123";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName6() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "あいう１２３";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName7() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "abc123あいう１２３";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetCategoryName8() {
+		CategoryDTO dto =new CategoryDTO();
+		String expected = "abc123あいう１２３アイウ漢字";
+
+		dto.setCategoryName(expected);
+		String actual =dto.getCategoryName();
+
+		assertEquals(expected,actual);
+
+	}
+
+
+	@Test
+	public void testGetCategoryDescription1() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected ="null";
+
+		dto.setCategoryDescription(expected);
+		String actual = dto.getCategoryDescription();
+
+		assertEquals(expected,actual);
+
+
+	}
+	@Test
+	public void testGetCategoryDescription2() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+
+	@Test
+	public void testGetCategoryDescription3() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = " ";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+
+	@Test
+	public void testGetCategoryDescription4() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "　";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+
+	@Test
+	public void testGetCategoryDescription5() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "aaaaaa1100";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+
+	@Test
+	public void testGetCategoryDescription6() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "ああアア１２３";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+	@Test
+	public void testGetCategoryDescription7() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "ab12あｂ１２アア";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
+	}
+
+	@Test
+	public void testGetCategoryDescription8() {
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "ab12あｂ１２アア漢字";
+
+		dto.setCategoryDescription(expected);
+		assertThat(expected,(dto.getCategoryDescription()));
+
 	}
 
 	@Test
 	public void testGetInsertDate() {
-		fail("まだ実装されていません");
-	}
+		CategoryDTO dto = new CategoryDTO();
+		String expected= "null";
+		dto.setInsertDate(expected);
 
-	@Test
-	public void testSetInsertDate() {
-		fail("まだ実装されていません");
+		assertThat(expected,is(dto.getInsertDate()));
+
 	}
 
 	@Test
 	public void testGetUpdateDate() {
-		fail("まだ実装されていません");
+		CategoryDTO dto = new CategoryDTO();
+		String expected = "null";
+		dto.setUpdateDate(expected);
+
+		String actual=dto.getUpdateDate();
+		assertEquals(expected,actual);
+
 	}
 
-	@Test
-	public void testSetUpdateDate() {
-		fail("まだ実装されていません");
+
+	private void assertThat(String expected, String string) {
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
+
+	private String is(String insertDate) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+
+
 
 }
--- src/test/DestinationDTOTest.java
+++ src/test/DestinationDTOTest.java
@@ -4,86 +4,771 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.wagashi.DTO.DestinationDTO;
+
 public class DestinationDTOTest {
 
 	@Test
-	public void testGetUserId() {
-		fail("まだ実装されていません");
+	public void testGetUserId1() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "null";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetUserId() {
-		fail("まだ実装されていません");
+	public void testGetUserId2() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetFamilyName() {
-		fail("まだ実装されていません");
+	public void testGetUserId3() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = " ";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetFamilyName() {
-		fail("まだ実装されていません");
+	public void testGetUserId4() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "　";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetFirstName() {
-		fail("まだ実装されていません");
+	public void testGetUserId5() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "ab12";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetFirstName() {
-		fail("まだ実装されていません");
+	public void testGetUserId6() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "あｂ１２";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetFamilyNameKana() {
-		fail("まだ実装されていません");
+	public void testGetUserId7() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "ab12あｂ１２";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
+	}
+
+
+	@Test
+	public void testGetUserId8() {
+	DestinationDTO dto = new DestinationDTO();
+	String expected = "ab12あｂ１２ア漢字";
+	dto.setUserId(expected);
+
+	String actual = dto.getUserId();
+	assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetFamilyNameKana() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetFirstNameKana() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetFirstNameKana() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetAddress() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetAddress() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetTelNumber() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetTelNumber() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testGetEmail() {
-		fail("まだ実装されていません");
+	public void testGetFamilyName8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setFamilyName(expected);
+		String actual=dto.getFamilyName();
+
+		assertEquals(expected,actual);
+
 	}
 
 	@Test
-	public void testSetEmail() {
-		fail("まだ実装されていません");
+	public void testgetFirstName1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
 	}
+
+	@Test
+	public void testgetFirstName2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testgetFirstName8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setFirstName(expected);
+		String actual=dto.getFirstName();
+
+		assertEquals(expected,actual);
+
+	}
+	@Test
+	public void testGetFamilyNameKana1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFamilyNameKana8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setFamilyNameKana(expected);
+		String actual=dto.getFamilyNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetFirstNameKana8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setFirstNameKana(expected);
+		String actual=dto.getFirstNameKana();
+
+		assertEquals(expected,actual);
+
+	}
+	@Test
+	public void testGetAddress1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetAddress8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setAddress(expected);
+		String actual=dto.getAddress();
+
+		assertEquals(expected,actual);
+
+	}
+
+
+	@Test
+	public void testGetTelNumber1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetTelNumber8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setTelNumber(expected);
+		String actual=dto.getTelNumber();
+
+		assertEquals(expected,actual);
+
+	}
+
+
+	@Test
+	public void testGetEmail1() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =null;
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail2() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail3() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected =" ";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail4() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="　";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail5() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail6() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="あいう１２３";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail7() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+	@Test
+	public void testGetEmail8() {
+		DestinationDTO dto = new DestinationDTO();
+		String expected ="abc123あいう１２３アイウ漢字";
+
+		dto.setEmail(expected);
+		String actual=dto.getEmail();
+
+		assertEquals(expected,actual);
+
+	}
+
+
 
 }
